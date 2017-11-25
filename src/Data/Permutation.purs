e84c14a0343471da94566e55e1445b64791fdd70
Vanessa McHale <vanessa.mchale@reconfigure.io>
2017-11-24 23:21:34 -0600

    initial commit

---
+++ src/Data/Permutation.purs
@@ -0,0 +1,69 @@
+module Data.Permutation ( P (..)
+                        , toArray
+                        , identity
+                        , pi
+                        , multiply
+                        ) where
+
+import Prelude
+import Data.Array (uncons, (:), index)
+import Data.Maybe
+
+data P n = Nil | Cons n (P n)
+
+derive instance eqP :: Eq n => Eq (P n)
+
+type Permutation = P Int
+
+instance showP :: Show (P Int) where
+    show Nil = "Nil"
+    show (Cons x xs) = "(Cons " <> show x <> " " <> show xs <> ")"
+
+identity :: Int -> Permutation
+identity 0 = Nil
+identity n = Cons 0 (identity $ n - 1)
+
+insert :: Int -> Array Int -> Int -> Array Int
+insert def l 0 = def : l
+insert def [] _ = [def]
+insert def l k = case uncons l of
+    (Just { head: x, tail: xs }) -> x : insert def xs (k - 1)
+    Nothing -> [] -- impossible
+
+sigma :: Permutation -> Array Int -> Array Int
+sigma Nil _ = []
+sigma (Cons p ps) l = case uncons l of
+    (Just { head: x, tail: xs }) -> insert x (sigma ps xs) p
+    Nothing -> []
+
+toArray :: Int -> Permutation -> Array Int
+toArray n p = sigma p (sequential n)
+    where sequential :: Int -> Array Int
+          sequential 0 = []
+          sequential m = 0 : map (add 1) (sequential $ m - 1)
+
+delete :: Int -> Int -> Permutation -> Permutation
+delete _ 0 (Cons j p) = p
+delete 0 _ _ = Nil
+delete n i (Cons 0 p) = Cons 0 (delete (n-1) (i-1) p)
+delete n i (Cons j p) = Cons (damp j) (delete (n-1) i p)
+    where damp :: Int -> Int
+          damp 0 = 0
+          damp n = n - 1
+delete _ _ _ = Nil -- shouldn't ever happen
+
+multiply :: Int -> Permutation -> Permutation -> Permutation
+multiply _ Nil p = p
+multiply n (Cons i p) p' = case index (toArray n p') i of
+    Just x -> Cons x (multiply (n-1) p (delete n i p'))
+    Nothing -> Nil -- shouldn't ever happen
+
+fill :: Int -> Int -> Permutation
+fill n 0 = identity n
+fill n k = Cons 1 (fill (n-1) $ k - 1)
+
+pi :: Int -> Int -> Int -> Permutation
+pi n 0 0 = identity n
+pi n 0 k = Cons k (fill (n-1) $ k - 1)
+pi n j 0 = Cons j (fill (n-1) $ j - 1)
+pi n j k = Cons 0 (pi (n-1) (j-1) (k-1))
---
+++ test/Main.purs
@@ -0,0 +1,19 @@
+module Test.Main where
+
+import Prelude
+
+import Test.Unit (suite, test)
+import Test.Unit.Main (runTest)
+import Test.Unit.Assert (assert, equal)
+
+import Data.Permutation
+
+main = runTest do
+  suite "permutation" do
+    test "basics" do
+      [0, 1, 2, 3] `equal` toArray 4 (identity 4)
+    test "pi" do
+      [0, 3, 2, 1] `equal` toArray 4 (pi 4 1 3)
+      [2, 1, 0, 3] `equal` toArray 4 (pi 4 0 2)
+    test "multiply" do
+      [2, 3, 0, 1] `equal` toArray 4 (multiply 4 (pi 4 1 3) (pi 4 0 2))
